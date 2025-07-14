be31df435c5f83385b4b715c268d6d40018eb0db
MiddlewareTalent <yugandhar@middlewaretalents.com>
2025-07-14 17:30:58 +0530

    Update UniversalApi.jsx

--- src/UniversalApi.jsx
+++ src/UniversalApi.jsx
@@ -1,4 +1,3 @@
 // const url="http://localhost:8080";
-//const url="https://companyproductbackend.azurewebsites.net";
-const url="https://mtlbackend.orangecliff-9a1af0ef.westus2.azurecontainerapps.io";
+const url="https://companyproductbackend.azurewebsites.net";
 export default url;
